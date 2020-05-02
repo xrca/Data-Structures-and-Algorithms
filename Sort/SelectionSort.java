c36116b8b021f3067f13974b5e712a352eb53bdc
xrca <fishsaury@163.com>
2020-05-02 11:36:18 +0800

    完善注释

--- Sort/SelectionSort.java
+++ Sort/SelectionSort.java
@@ -1,5 +1,10 @@
 package Sort;
 
+/**
+ * 选择排序
+ * @author xrca
+ * @date 2020-05-02 11:10
+ */
 public class SelectionSort {
     public static void main(String[] args) {
         // 获取随机数组
@@ -19,6 +24,7 @@ public class SelectionSort {
      */
     public static void sort(int[] array) {
         int minPos = 0;
+        // 每次循环结束，则前部排序完成的位置加1，下一次从此位置的下一个位置开始排序即可
         for (int i = 0; i < array.length - 1; i++) {
             minPos = i;
             for (int j = i + 1; j < array.length; j++) {
